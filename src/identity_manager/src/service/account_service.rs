26ccbcd7a8210537492ec85bcfa80be8b1275a60
oleksis <oleksis@jfrog.com>
2021-12-31 11:55:07 +0200

    Add application/app_name [sc-485]

--- src/identity_manager/src/service/account_service.rs
+++ src/identity_manager/src/service/account_service.rs
@@ -1,23 +1,24 @@
 use crate::{HttpResponse, TOKEN_STORAGE};
-use crate::http::requests::AccountRR;
+use crate::http::requests::AccountResponse;
 use crate::mapper::account_mapper::account_to_account_response;
 use crate::repository::repo::{Account, AccountRepo, calculate_hash, Device, Persona};
 use crate::requests::{HTTPAccountRequest, HTTPAccountUpdateRequest};
 use crate::response_mapper::to_error_response;
 use crate::response_mapper::to_success_response;
 
-pub fn get_account() -> HttpResponse<AccountRR> {
+pub fn get_account() -> HttpResponse<AccountResponse> {
     match AccountRepo::get_account() {
         Some(content) => to_success_response(account_to_account_response(content.clone())),
         None => to_error_response("Unable to find Account")
     }
 }
 
-pub fn create_account(account_request: HTTPAccountRequest) -> HttpResponse<AccountRR> {
+pub fn create_account(account_request: HTTPAccountRequest) -> HttpResponse<AccountResponse> {
     match validate_token(&account_request) {
         Ok(_) => (),
         Err(message) => return to_error_response(message)
     };
+    //todo validate on existence
 
     let princ = &ic_cdk::api::caller().to_text();
     let devices: Vec<Device> = Vec::new();
@@ -31,12 +32,13 @@ pub fn create_account(account_request: HTTPAccountRequest) -> HttpResponse<Accou
         email: account_request.email.clone(),
         devices,
         personas,
+        is_seed_phrase_copied: account_request.is_seed_phrase_copied
     };
     AccountRepo::store_account(acc.clone());
     to_success_response(account_to_account_response(acc))
 }
 
-pub fn update_account(account_request: HTTPAccountUpdateRequest) -> HttpResponse<AccountRR> {
+pub fn update_account(account_request: HTTPAccountUpdateRequest) -> HttpResponse<AccountResponse> {
     match AccountRepo::get_account() {
         Some(acc) => {
             let mut new_acc = acc.clone();
@@ -48,6 +50,9 @@ pub fn update_account(account_request: HTTPAccountUpdateRequest) -> HttpResponse
             }
             if !account_request.name.is_none() {
                 new_acc.name = account_request.name.unwrap();
+            }
+            if !account_request.is_seed_phrase_copied.is_none() {
+                new_acc.is_seed_phrase_copied = account_request.is_seed_phrase_copied.unwrap();
             }
             AccountRepo::store_account(new_acc.clone());
             to_success_response(account_to_account_response(new_acc))
