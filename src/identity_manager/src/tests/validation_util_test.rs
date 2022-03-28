330f902c9066d92db8eedf4752fe6eeb9ecf27ba
olsemeno <sendtosemenov@gmail.com>
2022-03-28 12:29:34 +0300

    Admin lib

--- src/identity_manager/src/tests/validation_util_test.rs
+++ src/identity_manager/src/tests/validation_util_test.rs
@@ -14,8 +14,8 @@ fn validate_name_test() {
 #[test]
 fn validate_persona_util_test() {
     let p_nfid = PersonaRequest {
-        domain: "Domain".to_string(),
-        persona_id: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_string(),
+        persona_id: "Domain".to_string(),
+        domain: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_string(),
     };
     assert!(!validate_frontend_length(&p_nfid));
     let p_nfid_1 = PersonaRequest {
