ebe6e2041254cff42438b107b0c12cbbf6c66ec7
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-05 07:26:48 +0200

    fix: build on m1 (#60)
    * fix: build on m1
    
    * Fix "ic0_msg_caller_size"
    
    Co-authored-by: olsemeno <sendtosemenov@gmail.com>

--- src/identity_manager/build.sh
+++ src/identity_manager/build.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-II_DIR="$(dirname "$0")"
+# Checking for dependencies
+# XXX: we currently cannot check for the exact version of ic-cdk-optimizer
+# because of https://github.com/dfinity/cdk-rs/issues/181
+# Once the issue is fixed, we can ensure that the correct version is installed
+if ! command -v ic-cdk-optimizer; then
+  echo could not find ic-cdk-optimizer
+  echo "ic-cdk-optimizer version 0.3.1 is needed, please run the following command:"
+  cargo install ic-cdk-optimizer --version 0.3.1
+  exit 1
+fi
+
+
+REPO_DIR="$(dirname "$0")"
 TARGET="wasm32-unknown-unknown"
 
-cargo build --manifest-path "$II_DIR/Cargo.toml" --target $TARGET --release -j1
+cargo_build_args=(
+  --manifest-path "$REPO_DIR/Cargo.toml"
+  --target "$TARGET"
+  --release
+  -j1
+)
 
-# keep version in sync with Dockerfile
-cargo install ic-cdk-optimizer --version 0.3.1 --root "$II_DIR"/../../target
-STATUS=$?
+echo Running cargo build "${cargo_build_args[@]}"
 
-if [ "$STATUS" -eq "0" ]; then
-      "$II_DIR"/../../target/bin/ic-cdk-optimizer \
-      "$II_DIR/../../target/$TARGET/release/identity_manager.wasm" \
-      -o "$II_DIR/../../target/$TARGET/release/identity_manager.wasm"
+cargo build "${cargo_build_args[@]}"
 
-  true
-else
-  echo Could not install ic-cdk-optimizer.
-  false
-fi
+CARGO_TARGET_DIR="${CARGO_TARGET_DIR:-$REPO_DIR/../../target/}"
+
+ic-cdk-optimizer \
+  "$CARGO_TARGET_DIR/$TARGET/release/identity_manager.wasm" \
+  -o "$REPO_DIR/../../identity_manager.wasm"
