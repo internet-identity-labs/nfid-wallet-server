8dc1079ca7bbb3e2941f6f74e0407ae4c28b5851
olsemeno <sendtosemenov@gmail.com>
2022-04-19 15:51:40 +0300

    Add hasPoap() method on user profile that returns boolean (false by default) [sc-1405]

---
+++ src/im_addition/src/main.rs
@@ -0,0 +1,44 @@
+use std::cell::RefCell;
+use std::collections::{BTreeMap, HashSet};
+
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::{storage, trap};
+use ic_cdk_macros::*;
+use ic_cdk::export::Principal;
+
+pub type ProofOfAttendanceProtocol = HashSet<Principal>;
+
+
+#[query]
+async fn ping() -> () {}
+
+#[query]
+async fn has_poap() -> bool {
+    let princ = ic_cdk::api::caller();
+    let index = storage::get_mut::<ProofOfAttendanceProtocol>();
+    return index.contains(&princ);
+}
+
+#[update]
+async fn increment_poap() {
+    let princ = ic_cdk::api::caller();
+    let index = storage::get_mut::<ProofOfAttendanceProtocol>();
+    index.insert(princ);
+}
+
+#[pre_upgrade]
+async fn pre_upgrade() {
+    storage::stable_save((storage::get_mut::<ProofOfAttendanceProtocol>(), 0));
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let poap: (HashSet<Principal>, i32) = storage::stable_restore().unwrap();
+    let mut index = storage::get_mut::<ProofOfAttendanceProtocol>();
+    for mut p in poap.0.iter() {
+        index.insert(p.to_owned());
+    }
+}
+
+
+fn main() {}
