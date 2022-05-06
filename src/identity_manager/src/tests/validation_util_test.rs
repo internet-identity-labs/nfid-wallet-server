577d5d3b8909103d912adf77b611dc94507d70d4
olsemeno <sendtosemenov@gmail.com>
2022-05-06 14:55:25 +0300

    Implement personaName field in the persona interface. [sc-1596]

--- src/identity_manager/src/tests/validation_util_test.rs
+++ src/identity_manager/src/tests/validation_util_test.rs
@@ -16,10 +16,12 @@ fn validate_persona_util_test() {
     let p_nfid = PersonaRequest {
         persona_id: "Domain".to_string(),
         domain: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_string(),
+        persona_name: "".to_string()
     };
     assert!(!validate_frontend_length(&p_nfid));
     let p_nfid_1 = PersonaRequest {
         domain: "Domain".to_string(),
+        persona_name: "".to_string(),
         persona_id: "Persona".to_string(),
     };
     assert!(validate_frontend_length(&p_nfid_1));
