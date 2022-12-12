08c974804978fdbb3111e5cd94f4310b188d0018
olsemeno <sendtosemenov@gmail.com>
2022-12-12 16:12:19 +0400

    Create some dummy methods from our Demo Application to test batchTransactions [sc-5160]

--- test/vault.test.ts
+++ test/vault.test.ts
@@ -2,253 +2,124 @@ import "mocha";
 import {deploy} from "./util/deployment.util";
 import {Dfx} from "./type/dfx";
 import {App} from "./constanst/app.enum";
-import {
-    Policy,
-    PolicyRegisterRequest, ThresholdPolicy,
-    Vault,
-    VaultMember,
-    VaultMemberRequest,
-    VaultRegisterRequest,
-    Wallet,
-    WalletRegisterRequest
-} from "./idl/vault";
+import {Vault, VaultMember,} from "./idl/vault";
 import {expect} from "chai";
 import {principalToAddress} from "ictool"
 import {DFX} from "./constanst/dfx.const";
 
+let rootAddress: string;
+let memberAddress: string;
 
 describe("Vault", () => {
-    var dfx: Dfx;
+    let dfx: Dfx;
+
 
     before(async () => {
         dfx = await deploy(App.Vault);
+
+        rootAddress = principalToAddress(
+            dfx.user.identity.getPrincipal() as any,
+            Array(32).fill(1));
+
+        memberAddress = principalToAddress(
+            dfx.vault.member.getPrincipal() as any,
+            Array(32).fill(1));
     });
 
     after(() => {
         DFX.STOP();
     });
 
-
-    it("vault get empty", async function () {
+    it("get_vaults empty", async function () {
         let vaults = await dfx.vault.actor.get_vaults() as [Vault]
         expect(vaults.length).eq(0)
     });
 
 
-    it("vault  register", async function () {
-        let request: VaultRegisterRequest = {
-            description: ["test"],
-            name: "vault1"
-        };
+    it("register_vault", async function () {
 
-        let vault = await dfx.vault.actor.register_vault(request) as Vault
-        let address = principalToAddress(dfx.user.identity.getPrincipal() as any, Array(32).fill(1));
-        expect(vault.name).eq("vault1")
-        expect(vault.members.length).eq(1)
-        expect(vault.members[0].user_uuid).eq(address)
-        expect(vault.members[0].role.hasOwnProperty('Admin')).eq(true)
-        request = {
+        let vault: Vault = await dfx.vault.actor.register_vault({
+            description: [],
+            name: "vault1"
+        }) as Vault
+        let expectedVault = getExpectedVault()
+
+        verifyVault(vault, expectedVault)
+        expect(vault.modified_date === vault.created_date).true
+
+        let vaultRegisterRequest2 = {
             description: ["test2"],
             name: "vault2"
         };
-        vault = await dfx.vault.actor.register_vault(request) as Vault
-        expect(vault.name).eq("vault2")
-        expect(vault.members.length).eq(1)
-        expect(vault.members[0].user_uuid).eq(address)
-        expect(vault.members[0].role.hasOwnProperty('Admin')).eq(true)
+        let vault2 = await dfx.vault.actor.register_vault(vaultRegisterRequest2) as Vault
+
+        expectedVault.name = "vault2";
+        expectedVault.id = 2n;
+        expectedVault.description = ["test2"];
+
+        verifyVault(vault2, expectedVault)
     });
 
-    it("vault  get by ids", async function () {
+    it("get_vaults", async function () {
         let vaults = await dfx.vault.actor.get_vaults() as [Vault]
-        expect(vaults.length).eq(2)
-        expect(vaults[0].id).eq(2n)
-        // @ts-ignore
-        expect(vaults[1].id).eq(1n)
+        let vault: Vault = vaults.find(l => l.id === 1n);
+        let vault2: Vault = vaults.find(l => l.id === 2n);
+        let expected = getExpectedVault();
+        verifyVault(vault, expected)
+
+        expected.name = "vault2";
+        expected.id = 2n;
+        expected.description = ["test2"];
+        verifyVault(vault2, expected)
     });
 
-    it("add member/ get members", async function () {
-        let memberAddress = principalToAddress(dfx.vault.member.getPrincipal() as any, Array(32).fill(1));
+    it("add_member", async function () {
 
-        let vaultMember: VaultMemberRequest = {
+        let vault = await dfx.vault.actor.add_vault_member({
             address: memberAddress,
             name: ["MoyaLaskovayaSuchechka"],
             role: {'Member': null},
             vault_id: 1n
-        }
-        let vault = await dfx.vault.actor.add_vault_member(vaultMember) as Vault;
-        let actual = vault.members.find(l => l.user_uuid === memberAddress);
-        expect(actual.name[0]).eq("MoyaLaskovayaSuchechka")
-        expect(actual.role.hasOwnProperty('Member')).eq(true)
+        }) as Vault;
 
-        let vaultForMember = (await dfx.vault.actor_member.get_vaults())[0] as Vault;
-        expect(vaultForMember.name).eq(vault.name)
-        expect(vaultForMember.id).eq(vault.id)
-        expect(vaultForMember.wallets.length).eq(vault.wallets.length)
-        expect(vaultForMember.policies.length).eq(vault.policies.length)
-        expect(vaultForMember.members.length).eq(2)
-        expect(vaultForMember.members[0].user_uuid).eq(vault.members[0].user_uuid)
-        expect(vaultForMember.members[1].user_uuid).eq(vault.members[1].user_uuid)
+        let expected = getExpectedVault()
+        expected.members.push(getDefaultMember())
+        verifyVault(vault, expected)
+        expect(vault.modified_date > vault.created_date).true
 
-        let membersForAdmin = (await dfx.vault.actor.get_vaults() as [Vault])
-            .filter(l => l.id === 1n)[0].members
-        let membersForMember = (await dfx.vault.actor_member.get_vaults() as [Vault])
-            .filter(l => l.id === 1n)[0].members
-        expect(membersForAdmin.length).eq(2)
-        expect(membersForMember.length).eq(2)
-        expect(membersForMember[0].user_uuid).eq(membersForAdmin[0].user_uuid)
-        expect(JSON.stringify(membersForMember[0].role)).eq(JSON.stringify(membersForAdmin[0].role))
-        // @ts-ignore
-        expect(membersForMember[1].user_uuid).eq(membersForAdmin[1].user_uuid)
-        // @ts-ignore
-        expect(JSON.stringify(membersForMember[1].role)).eq(JSON.stringify(membersForAdmin[1].role))
+        let vaultsForMember = (await dfx.vault.actor_member.get_vaults()) as [Vault];
+        expect(vaultsForMember.length).eq(1)
+        let vaultForMember = vaultsForMember[0]
+        verifyVault(vaultForMember, expected)
+
+        let vaultAdmin = (await dfx.vault.actor.get_vaults() as [Vault])
+            .find(l => l.id === 1n)
+        verifyVault(vaultAdmin, expected)
+
+        let vaultMember = (await dfx.vault.actor_member.get_vaults() as [Vault])
+            .find(l => l.id === 1n)
+        verifyVault(vaultMember, expected)
     });
+
+
     it("negative scenarios for add members", async function () {
-        let memberAddress = principalToAddress(dfx.vault.member.getPrincipal() as any, Array(32).fill(1));
-        let vaultMember: VaultMemberRequest = {
-            address: memberAddress,
-            name: ["Moya Laskovaya Suchechka"],
-            role: {'Member': null},
-            vault_id: 2n
-        }
         try {
-            await dfx.vault.actor_member.add_vault_member(vaultMember)
-        } catch (e: any) {
-            expect(e.message.includes("Unauthorised")).eq(true)
-        }
-        vaultMember = {
-            address: memberAddress,
-            name: ["Moya Laskovaya Suchechka"],
-            role: {'Member': null},
-            vault_id: 1n
-        }
-        try {
-            await dfx.vault.actor_member.add_vault_member(vaultMember)
-        } catch (e: any) {
-            expect(e.message.includes("Not enough permissions")).eq(true)
-        }
-    });
-    it("register wallet", async function () {
-        let request: WalletRegisterRequest = {name: ["Wallet1"], vault_id: 1n};
-        let result = await dfx.vault.actor.register_wallet(request) as Wallet
-        expect(result.name[0]).eq("Wallet1")
-        expect(result.vaults[0]).eq(1n)
-        expect(result.id).eq(1n)
-        request = {name: ["Wallet2"], vault_id: 1n};
-        result = await dfx.vault.actor.register_wallet(request) as Wallet
-        expect(result.name[0]).eq("Wallet2")
-        expect(result.vaults[0]).eq(1n)
-        expect(result.id).eq(2n)
-        let wallets = await dfx.vault.actor.get_wallets(1n) as [Wallet]
-        expect(wallets.length).eq(2)
-        expect(wallets[0].id).eq(1n)
-        // @ts-ignore
-        expect(wallets[1].id).eq(2n)
-    });
-
-    it("register wallet negative ", async function () {
-        try {
-            await dfx.vault.actor_member.get_wallets(2n)
+            await dfx.vault.actor_member.add_vault_member({
+                address: memberAddress,
+                name: ["Moya Laskovaya Suchechka"],
+                role: {'Member': null},
+                vault_id: 2n
+            })
         } catch (e: any) {
             expect(e.message.includes("Unauthorised")).eq(true)
         }
         try {
-            await dfx.vault.actor_member.get_wallets(3n)
-        } catch (e: any) {
-            console.log(e)
-            expect(e.message.includes("Nonexistent key")).eq(true)
-        }
-        let request: WalletRegisterRequest = {name: ["Wallet1"], vault_id: 3n};
-        try {
-            await dfx.vault.actor_member.register_wallet(request) as Wallet
-        } catch (e: any) {
-            expect(e.message.includes("Nonexistent key")).eq(true)
-        }
-
-        request = {name: ["Wallet1"], vault_id: 2n};
-        try {
-            await dfx.vault.actor_member.register_wallet(request) as Wallet
-        } catch (e: any) {
-            expect(e.message.includes("Unauthorised")).eq(true)
-        }
-
-        request = {name: ["Wallet1"], vault_id: 1n};
-        try {
-            await dfx.vault.actor_member.register_wallet(request) as Wallet
-        } catch (e: any) {
-            expect(e.message.includes("Not enough permissions")).eq(true)
-        }
-    });
-
-
-    it("register policy", async function () {
-        let tp: ThresholdPolicy = {
-            amount_threshold: 10n,
-            currency: {'ICP': null},
-            member_threshold: 2,
-            wallet_ids: []
-        }
-        let request: PolicyRegisterRequest = {policy_type: {'threshold_policy': tp}, vault_id: 1n};
-
-        let result = await dfx.vault.actor.register_policy(request) as Policy
-        expect(result.id).eq(1n)
-        expect(result.policy_type.threshold_policy.amount_threshold).eq(10n)
-        expect(result.policy_type.threshold_policy.member_threshold).eq(2)
-        expect(result.policy_type.threshold_policy.wallet_ids.length).eq(0)
-        expect(hasOwnProperty(result.policy_type.threshold_policy.currency, 'ICP')).eq(true)
-        tp = {
-            amount_threshold: 2n,
-            currency: {'ICP': null},
-            member_threshold: 3,
-            wallet_ids: []
-        }
-        request = {policy_type: {'threshold_policy': tp}, vault_id: 1n};
-
-        result = await dfx.vault.actor.register_policy(request) as Policy
-        expect(result.id).eq(2n)
-        let policies = await dfx.vault.actor.get_policies(1n) as [Policy]
-        expect(policies.length).eq(2)
-        expect(policies[0].id).eq(1n)
-        // @ts-ignore
-        expect(policies[1].id).eq(2n)
-    });
-
-
-    it("register policy negative ", async function () {
-        try {
-            await dfx.vault.actor_member.get_policies(2n)
-        } catch (e: any) {
-            expect(e.message.includes("Unauthorised")).eq(true)
-        }
-        try {
-            await dfx.vault.actor_member.get_policies(3n)
-        } catch (e: any) {
-            expect(e.message.includes("Nonexistent key error")).eq(true)
-        }
-
-        let tp: ThresholdPolicy = {
-            amount_threshold: 10n,
-            currency: {'ICP': null},
-            member_threshold: 2,
-            wallet_ids: []
-        }
-        let request: PolicyRegisterRequest = {policy_type: {'threshold_policy': tp}, vault_id: 3n};
-
-        try {
-            await dfx.vault.actor_member.register_policy(request)
-        } catch (e: any) {
-            expect(e.message.includes("Nonexistent key")).eq(true)
-        }
-
-        request = {policy_type: {'threshold_policy': tp}, vault_id: 2n};
-        try {
-            await dfx.vault.actor_member.register_policy(request)
-        } catch (e: any) {
-            expect(e.message.includes("Unauthorised")).eq(true)
-        }
-
-        request = {policy_type: {'threshold_policy': tp}, vault_id: 1n};
-        try {
-            await dfx.vault.actor_member.register_policy(request)
+            await dfx.vault.actor_member.add_vault_member({
+                address: memberAddress,
+                name: ["Moya Laskovaya Suchechka"],
+                role: {'Member': null},
+                vault_id: 1n
+            })
         } catch (e: any) {
             expect(e.message.includes("Not enough permissions")).eq(true)
         }
@@ -256,9 +127,57 @@ describe("Vault", () => {
 });
 
 
-// A `hasOwnProperty` that produces evidence for the typechecker
-export function hasOwnProperty<X extends Record<string, unknown>,
-    Y extends PropertyKey,
-    >(obj: X, prop: Y): obj is X & Record<Y, unknown> {
-    return Object.prototype.hasOwnProperty.call(obj, prop)
+function verifyVault(actualVault: Vault, expectedVault: Vault) {
+    expect(actualVault.name).eq(expectedVault.name)
+    expect(actualVault.id).eq(expectedVault.id)
+    expect(Object.keys(actualVault.state)[0]).eq(Object.keys(expectedVault.state)[0])
+    expect(actualVault.description.length).eq(expectedVault.description.length)
+    if (actualVault.description.length > 0) {
+        expect(actualVault.description[0]).eq(expectedVault.description[0])
+    }
+    // expect(actualVault.wallets).eq(expectedVault.wallets)
+    // expect(actualVault.policies).eq(expectedVault.policies)
+    expect(actualVault.modified_date !== 0n).true
+    verifyMember(actualVault.members[0], expectedVault.members[0])
+
+}
+
+function verifyMember(actual: VaultMember, expected: VaultMember) {
+    expect(actual.name.length).eq(expected.name.length)
+    if (actual.name.length > 0) {
+        expect(actual.name[0]).eq(expected.name[0])
+    }
+    expect(actual.user_uuid).eq(expected.user_uuid)
+    expect(Object.keys(actual.state)[0]).eq(Object.keys(expected.state)[0])
+    expect(Object.keys(actual.role)[0]).eq(Object.keys(expected.role)[0])
+}
+
+function getExpectedVault(): Vault {
+    return {
+        created_date: 0n,
+        description: [],
+        id: 1n,
+        members: [
+            {
+                name: [],
+                role: {'Admin': null},
+                state: {'Active': null},
+                user_uuid: rootAddress
+            }
+        ],
+        modified_date: 0n,
+        name: 'vault1',
+        policies: [],
+        state: {'Active': null},
+        wallets: []
+    }
+}
+
+function getDefaultMember(): VaultMember {
+    return {
+        state: undefined,
+        user_uuid: memberAddress,
+        name: ["MoyaLaskovayaSuchechka"],
+        role: {'Member': null},
+    }
 }
