bad74fffa187ac6dc52ce6bc0b955cf4ff49f043
olsemeno <33149496+olsemeno@users.noreply.github.com>
2023-02-03 15:51:52 +0200

    [Backend] Optimistically sign a transaction and keep it in canister until user approves [sc-6233] (#176)
    * [Backend] Optimistically sign a transaction and keep it in canister until user approves [sc-6233]
    
    * [Backend] Optimistically sign a transaction and keep it in canister until user approves [sc-6233]

--- test/ecdsa-signer.test.ts
+++ test/ecdsa-signer.test.ts
@@ -17,10 +17,8 @@ describe("ECDSA signer test", () => {
         after(() => {
             DFX.STOP();
         });
-
         it("should return public key", async function () {
             let pk = await dfx.ecdsa.actor.public_key() as Result;
-            console.log(pk)
             // @ts-ignore
             expect(pk.Ok.public_key.length > 0).eq(true)
         });
@@ -33,5 +31,34 @@ describe("ECDSA signer test", () => {
             expect(signature.Ok.signature.length).eq(64)
         });
 
+        it("should prepare signature", async function () {
+            let message = Array(32).fill(1);
+            let key = await dfx.ecdsa.actor.prepare_signature(message) as String;
+            expect(key).eq("0101010101010101010101010101010101010101010101010101010101010101")
+            let signature = await dfx.ecdsa.actor.get_signature(key) as Result_1;
+            console.log(signature)
+            // @ts-ignore
+            expect(signature.Ok.signature.length).eq(64)
+            let signature2 = await dfx.ecdsa.actor.get_signature(key) as Result_1;
+            console.log(signature2)
+            // @ts-ignore
+            expect(assertArray(signature.Ok.signature, signature2.Ok.signature)).eq(true)
+            await new Promise(r => setTimeout(r, 3000));
+            await dfx.ecdsa.actor.public_key() as Result; //run cleanup
+            let signature3 = await dfx.ecdsa.actor.get_signature(key) as Result_1;
+            // @ts-ignore
+            expect(signature3.Err).eq('No such signature')
+        });
     });
+
+    function assertArray(a: [], b: []) {
+        let i = 0;
+        while (i < 64) {
+            if (a[i] !== b[i]) {
+                return false
+            }
+            i++
+        }
+        return true
+    }
 });
