01a13e65b1b98887e5bf10125637f8817779ed7d
olsemeno <sendtosemenov@gmail.com>
2023-09-28 08:54:55 +0200

    [Backend] Implement certified query calls for origins [sc-9021]

--- src/test_canister/src/lib.rs
+++ src/test_canister/src/lib.rs
@@ -1,29 +1,65 @@
 use ic_cdk_macros::{query, update};
 use std::cell::RefCell;
+use ic_cdk::api::set_certified_data;
 use ic_cdk::caller;
-use ic_cdk::export::candid::{candid_method, export_service};
+use ic_cdk::export::candid::{candid_method, export_service, CandidType};
+use ic_cdk::export::{
+    serde::{Serialize},
+};
+use ic_cdk_macros::*;
+use ic_certified_map::{AsHashTree, RbTree};
 
+
+#[derive(CandidType)]
+struct CertifiedResponse {
+    response: Vec<String>,
+    certificate: Vec<u8>,
+    witness: Vec<u8>,
+}
 
 thread_local! {
     static ORIGIN_STORAGE: RefCell<Vec<String>> = RefCell::new(Default::default());
+    static TREE: RefCell<RbTree<String, Vec<u8>>> = RefCell::new(RbTree::new());
 }
-
 
 
 #[update]
 #[candid_method(update)]
 async fn get_trusted_origins() -> Vec<String> {
     ORIGIN_STORAGE.with(|storage| {
-         storage.borrow().clone()
+        storage.borrow().clone()
     })
+}
+
+#[query]
+#[candid_method(query)]
+async fn get_trusted_origins_certified() -> CertifiedResponse {
+    let witness = match get_count_witness("origins".to_string()) {
+        Ok(tree) => tree,
+        Err(_) => {
+            Vec::default()
+        }
+    };
+    let origins = ORIGIN_STORAGE.with(|storage| {
+        storage.borrow().clone()
+    });
+
+    let certificate = ic_cdk::api::data_certificate().expect("No data certificate available");
+
+    CertifiedResponse {
+        response: origins,
+        certificate,
+        witness,
+    }
 }
 
 
 #[update]
 #[candid_method(update)]
 async fn update_trusted_origins(a: Vec<String>) -> Vec<String> {
+    update_certify_keys("origins".to_string(), a.clone());
     ORIGIN_STORAGE.with(|storage| {
-         storage.replace(a);
+        storage.replace(a);
         storage.borrow().clone()
     })
 }
@@ -35,10 +71,50 @@ async fn get_principal() -> String {
 }
 
 
+#[post_upgrade]
+async fn post_upgrade() {
+    let a: Vec<String> = vec!["http://localhost:4200".to_string(),
+                              "nfid.one".to_string(),
+                              "https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app".to_string(),
+                              "https://playground-dev.nfid.one".to_string(), ];
+    update_trusted_origins(a).await;
+}
+
+
+fn get_count_witness(key: String) -> anyhow::Result<Vec<u8>> {
+    TREE.with(|tree| {
+        let tree = tree.borrow();
+        let mut witness = vec![];
+        let mut witness_serializer = serde_cbor::Serializer::new(&mut witness);
+
+        witness_serializer.self_describe()?;
+
+        tree.witness(key.as_bytes())
+            .serialize(&mut witness_serializer)
+            .unwrap();
+
+        Ok(witness)
+    })
+}
+
 export_service!();
 
+fn update_certify_keys(key: String, origins: Vec<String>) -> String {
+    TREE.with(|k| {
+        let mut keys = k.borrow_mut();
+        let concatenated_string: String = origins.join("");
+        let b = hex::decode(sha256::digest(concatenated_string)).unwrap();
+        keys.insert(key.clone(), b);
+        set_certified_data(&keys.root_hash());
+        key
+    })
+}
 
 #[ic_cdk_macros::query(name = "__get_candid_interface_tmp_hack")]
 fn export_candid() -> String {
     __export_service()
 }
+
+
+#[test]
+fn build() {}
