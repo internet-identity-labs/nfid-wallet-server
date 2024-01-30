f511fb31b00c678c4da834cb25f6eb8ad5a441cd
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-01-30 16:07:35 +0200

    [boomDAO] support for ICRC tokens + security [sc-10552] (#246)

--- test/idl/icrc1_registry.ts
+++ test/idl/icrc1_registry.ts
@@ -1,7 +1,8 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface Conf { 'im_canister' : [] | [string] }
 export interface _SERVICE {
-  'add_icrc1_canister' : ActorMethod<[string], undefined>,
-  'get_canisters' : ActorMethod<[], Array<string>>,
+    'get_canisters_by_root' : ActorMethod<[string], Array<string>>,
+    'store_icrc1_canister' : ActorMethod<[string], undefined>,
 }
--- test/idl/icrc1_registry_idl.ts
+++ test/idl/icrc1_registry_idl.ts
@@ -1,7 +1,11 @@
 export const idlFactory = ({ IDL }) => {
     return IDL.Service({
-        'add_icrc1_canister' : IDL.Func([IDL.Text], [], []),
-        'get_canisters' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+        'get_canisters_by_root' : IDL.Func(
+            [IDL.Text],
+            [IDL.Vec(IDL.Text)],
+            ['query'],
+        ),
+        'store_icrc1_canister' : IDL.Func([IDL.Text], [], []),
     });
 };
 export const init = ({ IDL }) => { return []; };
