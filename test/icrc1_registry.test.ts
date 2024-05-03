514528cc9008dabfb885922c3536b69ef3b18505
olsemeno <sendtosemenov@gmail.com>
2024-05-03 14:57:38 +0200

    ICRC 1 Implement logic to add and read the [sc-12188]

--- test/icrc1_registry.test.ts
+++ test/icrc1_registry.test.ts
@@ -3,6 +3,7 @@ import {deploy} from "./util/deployment.util";
 import {App} from "./constanst/app.enum";
 import {DFX} from "./constanst/dfx.const";
 import {expect} from "chai";
+import {ICRC1} from "./idl/icrc1_registry";
 
 describe("ICRC1 canister Storage", () => {
     var dfx: Dfx;
@@ -17,12 +18,15 @@ describe("ICRC1 canister Storage", () => {
 
     it("Store/retrieve canister id", async function () {
         let canister_id = "id1";
+        let canister_index_id = "index1";
         let one_more_canister_id = "id2";
-        await dfx.icrc1.actor.store_icrc1_canister(canister_id);
-        await dfx.icrc1.actor.store_icrc1_canister(one_more_canister_id);
-        let canisters = await dfx.icrc1.actor.get_canisters_by_root(dfx.user.identity.getPrincipal().toText()) as string[];
+        await dfx.icrc1.actor.store_icrc1_canister(canister_id, [canister_index_id]);
+        await dfx.icrc1.actor.store_icrc1_canister(one_more_canister_id, []);
+        let canisters = await dfx.icrc1.actor.get_canisters_by_root(dfx.user.identity.getPrincipal().toText()) as ICRC1[];
         expect(canisters.length).eq(2);
-        expect(canisters[0]).eq(canister_id);
-        expect(canisters[1]).eq(one_more_canister_id);
+        expect(canisters[0].ledger).eq(canister_id);
+        expect(canisters[0].index[0]).eq(canister_index_id);
+        expect(canisters[1].ledger).eq(one_more_canister_id);
+        expect(canisters[1].index.length).eq(0);
     })
 })
--- test/idl/icrc1_registry.ts
+++ test/idl/icrc1_registry.ts
@@ -1,8 +1,11 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
 
 export interface Conf { 'im_canister' : [] | [string] }
+export interface ICRC1 { 'ledger' : string, 'index' : [] | [string] }
 export interface _SERVICE {
-    'get_canisters_by_root' : ActorMethod<[string], Array<string>>,
-    'store_icrc1_canister' : ActorMethod<[string], undefined>,
+    'get_canisters_by_root' : ActorMethod<[string], Array<ICRC1>>,
+    'store_icrc1_canister' : ActorMethod<[string, [] | [string]], undefined>,
 }
+export declare const idlFactory: IDL.InterfaceFactory;
