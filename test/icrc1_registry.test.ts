4e790dee47ef3f639f2a095321ea75294082389d
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-05-20 16:01:58 +0300

    ICRC 1 Implement a method to remove already [sc-12673] (#252)

--- test/icrc1_registry.test.ts
+++ test/icrc1_registry.test.ts
@@ -16,10 +16,11 @@ describe("ICRC1 canister Storage", () => {
         DFX.STOP();
     });
 
+    let canister_id = "id1";
+    let canister_index_id = "index1";
+    let one_more_canister_id = "id2";
+
     it("Store/retrieve canister id", async function () {
-        let canister_id = "id1";
-        let canister_index_id = "index1";
-        let one_more_canister_id = "id2";
         await dfx.icrc1.actor.store_icrc1_canister(canister_id, [canister_index_id]);
         await dfx.icrc1.actor.store_icrc1_canister(one_more_canister_id, []);
         let canisters = await dfx.icrc1.actor.get_canisters_by_root(dfx.user.identity.getPrincipal().toText()) as ICRC1[];
@@ -28,5 +29,12 @@ describe("ICRC1 canister Storage", () => {
         expect(canisters[0].index[0]).eq(canister_index_id);
         expect(canisters[1].ledger).eq(one_more_canister_id);
         expect(canisters[1].index.length).eq(0);
+    })
+
+    it("Remove canister", async function () {
+        await dfx.icrc1.actor.remove_icrc1_canister(canister_id);
+        let canisters = await dfx.icrc1.actor.get_canisters_by_root(dfx.user.identity.getPrincipal().toText()) as ICRC1[];
+        expect(canisters.length).eq(1);
+        expect(canisters[0].ledger).eq(one_more_canister_id);
     })
 })
--- test/idl/icrc1_registry.ts
+++ test/idl/icrc1_registry.ts
@@ -6,6 +6,7 @@ export interface Conf { 'im_canister' : [] | [string] }
 export interface ICRC1 { 'ledger' : string, 'index' : [] | [string] }
 export interface _SERVICE {
     'get_canisters_by_root' : ActorMethod<[string], Array<ICRC1>>,
+    'remove_icrc1_canister' : ActorMethod<[string], undefined>,
     'store_icrc1_canister' : ActorMethod<[string, [] | [string]], undefined>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
