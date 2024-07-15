fdd2f3ee1e9945548e0bcc57c22d051cb1b5d35b
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-07-16 00:23:29 +0200

    Migrate the Passkey method from AWS to the [sc-12891] (#259)

--- test/idl/nfid_storage.ts
+++ test/idl/nfid_storage.ts
@@ -3,8 +3,9 @@ import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
 export interface InitArgs { 'im_canister' : Principal }
+export interface PassKeyData { 'data' : string, 'key' : string }
 export interface _SERVICE {
-  'get_passkey' : ActorMethod<[], [string]>,
-  'store_passkey' : ActorMethod<[string], bigint>,
+    'get_passkey' : ActorMethod<[Array<string>], Array<PassKeyData>>,
+    'store_passkey' : ActorMethod<[string, string], bigint>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
--- test/idl/nfid_storage_idl.ts
+++ test/idl/nfid_storage_idl.ts
@@ -1,7 +1,8 @@
 export const idlFactory = ({ IDL }) => {
-  const InitArgs = IDL.Record({ 'im_canister' : IDL.Principal });
-  return IDL.Service({
-    'get_passkey' : IDL.Func([], [IDL.Opt(IDL.Text)], ['query'],),
-    'store_passkey' : IDL.Func([IDL.Text], [IDL.Nat64], []),
-  });
+    const InitArgs = IDL.Record({ 'im_canister' : IDL.Principal });
+    const PassKeyData = IDL.Record({ 'data' : IDL.Text, 'key' : IDL.Text });
+    return IDL.Service({
+        'get_passkey' : IDL.Func([IDL.Vec(IDL.Text)], [IDL.Vec(PassKeyData)], ['query'],),
+        'store_passkey' : IDL.Func([IDL.Text, IDL.Text], [IDL.Nat64], []),
+    });
 };
