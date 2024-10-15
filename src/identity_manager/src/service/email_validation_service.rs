9bd48d7c45e3a3c6c3a373fbf040ccfbd06a1ef0
Dmitrii <dmitrii@identitylabs.ooo>
2024-10-15 19:27:55 +0400

    Implement additional validation logic for [sc-15388]

--- src/identity_manager/src/service/email_validation_service.rs
+++ src/identity_manager/src/service/email_validation_service.rs
@@ -1,16 +1,24 @@
 use crate::structure::ttl_hashmap::TtlHashMap;
+use crate::{HttpResponse, to_success_response};
 use std::cell::RefCell;
+use ic_cdk::trap;
 
 thread_local! {
     static TOKENS_REPOSITORY: RefCell<TtlHashMap<String, String>> = RefCell::new(TtlHashMap::new(900000));
 }
 
-pub fn insert(key: String, value: String, timestamp: u64) -> () {
+pub fn insert(key: String, value: String, timestamp: u64) -> HttpResponse<bool> {
+    if !is_valid_email_address_size(&key) {
+        trap("Incorrect email address size: it's more than 320 characters.");
+    }
+
     TOKENS_REPOSITORY.with(|repository| {
         let mut repo = repository.borrow_mut();
         repo.clean_expired_entries(timestamp);
         repo.insert(key, value, timestamp);
     });
+
+    to_success_response(true)
 } 
 
 pub fn contains(key: String, value: String) -> bool {
@@ -20,4 +28,8 @@ pub fn contains(key: String, value: String) -> bool {
             None => false,
         }
     })
+}
+
+fn is_valid_email_address_size(email: &str) -> bool {
+    email.len() <= 320
 }
