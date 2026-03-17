cd818c997cced71f9df9cdf866147825e018736c
Dmytro <dmytroidentitylabs@gmail.com>
2026-03-17 23:14:39 +0400

    New applications can be added by tracking [sc-19299]

--- test/discovery.test.ts
+++ test/discovery.test.ts
@@ -41,7 +41,7 @@ const ATOMIC_WALLET_APP: DiscoveryApp = {
 
 describe("DiscoveryService", () => {
   describe("getApps", () => {
-    it("should enrich apps with url, name, icon and desc", async () => {
+    it("should enrich apps with url, name, image and desc", async () => {
       // Given valid apps with reachable hostnames
       const request = [NFID_APP, KONG_SWAP_APP, UBIN_APP, ATOMIC_WALLET_APP];
 
@@ -56,7 +56,7 @@ describe("DiscoveryService", () => {
             ...NFID_APP,
             url: "https://nfid.one/",
             name: "NFID Wallet | The ICP Wallet",
-            icon: "https://nfid.one/assets/icons/apple-touch-icon.png",
+            image: "https://nfid.one/assets/nfid-wallet-og.png",
             desc: "The easiest to use, hardest to lose, and only wallet governed by a DAO.",
           },
         },
@@ -66,7 +66,7 @@ describe("DiscoveryService", () => {
             ...KONG_SWAP_APP,
             url: "https://kongswap.io/",
             name: "KongSwap - Internet Computer’s Leading DEX | Zero Gas Cross-Chain Trading",
-            icon: "https://kongswap.io/apple-touch-icon.png",
+            image: "https://kongswap.io/images/og-banner.png",
             desc: "The Internet Computer’s leading DEX for seamless, zero-gas crypto swaps. Swap ICP and SOL tokens with best rates and zero slippage.",
           },
         },
@@ -76,7 +76,7 @@ describe("DiscoveryService", () => {
             ...UBIN_APP,
             url: "https://h3cjw-syaaa-aaaam-qbbia-cai.ic0.app",
             name: "Welcome to uBin!",
-            icon: undefined,
+            image: undefined,
             desc: "The only cloud storage platform where you truly own your data.",
           },
         },
@@ -86,7 +86,7 @@ describe("DiscoveryService", () => {
             ...ATOMIC_WALLET_APP,
             url: "https://atomicwallet.io/",
             name: "Best Cryptocurrency Wallet for Mobile & Desktop: Bitcoin & Altcoins",
-            icon: "https://atomicwallet.io/images/logo.ico",
+            image: "https://atomicwallet.io/images/wallets/cryptocurrency_bitcoin_wallet-ios.png",
             desc: "Buy, stake, swap, and manage cryptocurrencies with the best Cryptocurrency Wallet & Bitcoin Wallet. Secure Atomic Wallet for your crypto assets and NFTs.",
           },
         },
